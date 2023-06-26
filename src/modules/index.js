2f163354ba9b8116a660d0e03965dc3d279c38a9
Alexander <47399778+AgentRa@users.noreply.github.com>
2023-06-26 15:44:15 +0200

    Delete src directory

--- src/modules/index.js
+++
@@ -1,7 +0,0 @@
-import * as navigation from "./navigation.js";
-import * as systemInfo from "./systemInfo.js";
-import * as crypto from "./crypto.js";
-import * as zip from "./zip.js";
-import * as fileSystem from "./fileSystem.js";
-
-export default { navigation, systemInfo, crypto, zip, fileSystem };
--- src/utils/index.js
+++
@@ -1,6 +0,0 @@
-import * as parse from "./parse.js";
-import * as path from "./path.js";
-import * as user from "./user.js";
-import * as validate from "./validate.js";
-
-export { parse, path, user, validate };
